6beada93a07c71870b50a1de6d17893a17b81cd3
zhanglei1949 <zhanglei1949@sjtu.edu.cn>
2015-06-12 23:11:15 +0800

    modify
    2015-6-12

--- MyList.cpp
+++ MyList.cpp
@@ -51,12 +51,12 @@ public:
 	*/
 	void erase(int start, int end);
 	// Get the item by calling index.
-	T get_item(int index);
+	T get_item(int index) ;
 	/* Return a MyList contains elements from 'start' index to 'end' index.
 	   Minus numbers are also supported.
 	   'Start' element and 'end' element are included. 
 	*/ 
-	MyList get_item(int start, int end);
+	MyList get_item(int start, int end) ;
 	// Return the number of the elements that has the same value as item.
 	int count(const T &item);
 	//Remove the first element in MyList that is equal to specified item.
@@ -342,6 +342,7 @@ void MyList <T>::remove(const T &item)
 template <class T>
 MyList<T> & MyList <T>::operator = (const MyList &l)
 {
+	if (this==&l) return *this; 
 	clean();
 	for (int i=0;i<l.get_size();++i){
         // cout<<ind<<" "<<"= "<<l.a[i]<<endl;
@@ -449,8 +450,36 @@ MyList <T> operator + (const MyList <T> &l1, const T &item)
     tmp.push(item);
     return tmp;
 }
-int main() 
-{
-	
+int main(){
+	MyList<int> a, b;
+	int i;
+	for (i=0; i<5; ++i)
+		a.push(i);
+    // a = [0, 1, 2, 3, 4]
+	a[3] = 15; // a = [0, 1, 2, 15, 4]
+	a.sort(); // a = [0, 1, 2, 4, 15]
+	a.reverse(); // a = [15, 4, 2, 1, 0]
+	a += 12; // a = [15, 4, 2, 1, 0, 12]
+	for (i=0; i<a.get_size(); ++i)
+		cout<<a[i]<<endl;
+    b = a.get_item(4, -3); // b = [] 
+	b = a.get_item(3, -1); // b = [1, 0, 12]
+	a += b; // a = [15, 4, 2, 1, 0, 12, 1, 0, 12]
+	for (i=0; i<a.get_size(); ++i)
+		cout<<a.get_item(i)<<endl;
+	cout<<a.count(5)<<endl;
+	b.clean(); // b = []
+	cout<<b.get_size()<<endl;
+	a.erase(2, 5); // a = [15, 4, 0, 12]
+	b = a + a; // b = [15, 4, 0, 12, 15, 4, 0, 12]
+	b.insert(3, 116); // b = [15, 4, 0, 116, 12, 15, 4, 0, 12]
+	b.remove(4); // b = [15, 0, 116, ...]
+	cout<<b<<endl;
+	MyList<double> c(10, 3.14);
+	for (i=0; i<100; ++i)
+		c.push(1.1*i);
+	cout<<c.get_item(100, 105)<<endl;
+
+	return 0;
 }
 
